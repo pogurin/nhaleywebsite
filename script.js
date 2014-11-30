94e1c57e73f52e0e1b0beb9130be7e7cf356d681
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-30 15:12:16 -0500

    Added semi-finished version of webpage

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+	$('#hideBox').fadeIn();
+
+	$(document).scroll(function () {
+		if ($(this).scrollTop() < $('#hideBox').height()) {
+			$('#hideBox').fadeIn();
+		} else {
+			$('#hideBox').fadeOut();
+		}
+	});
+
+	$("nav").hover(function(){
+		$(this).find("#hideBox").fadeIn();
+	}, function() {
+		$(this).find("#hideBox").fadeOut();
+	});
+
+	$("#pageTop").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#welcome").offset().top
+		}, 1000);
+	});
+
+	$("#aboutnav").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#info").offset().top
+		}, 1000);
+	});
+
+	$("#skillsnav").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#skills").offset().top
+		}, 1000);
+	});
+
+	$("#projectsnav").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#projects").offset().top
+		}, 1000);
+	});
+
+	$("#contactnav").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#contact").offset().top
+		}, 1000);
+	});
+
+	// $('#hideBox').blurjs({
+	// 	source: '#welcome',
+	// 	radius: 30,
+	// 	overlay: 'rgba(0, 0, 0, .2)'
+	// });
+});
